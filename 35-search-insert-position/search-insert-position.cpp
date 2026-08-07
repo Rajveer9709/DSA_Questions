91b55334b31ddd974a284d7e457bfa5cb4541dfd
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-07 23:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (44.07%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,16 +1,39 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] == target) {
+//                 return i;
+//             }
+//             if (nums[i] > target) {
+//                 return (i);
+//             }
+            
+//         }
+
+//         return nums.size();
+//     }
+// };
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                return i;
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            if (nums[i] > target) {
-                return (i);
-            }
-            
         }
 
-        return nums.size();
+        return left;        
     }
 };
