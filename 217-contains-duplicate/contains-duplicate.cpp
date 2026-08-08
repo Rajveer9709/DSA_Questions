2e57d56cf591b4f5658a064318d0f4dc78ae01c9
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-08 15:10:29 +0530

    Time: 15 ms (99.12%) | Memory: 82.9 MB (77.03%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -11,15 +11,30 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> seen;
+//         for (int num : nums) {
+//             if (seen.count(num) > 0)
+//                 return true;
+//             seen.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.count(num) > 0)
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
-            seen.insert(num);
+            }
         }
-        return false;
+        
+        return false;        
     }
 };
