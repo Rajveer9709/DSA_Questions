fa9e2c837601ea7fd370ba0dea7c4a73bcc4c524
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-12 17:24:51 +0530

    Time: 3 ms (28.48%) | Memory: 28.3 MB (42.70%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -17,29 +17,47 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         int left = 0;
+//         int right = 0;
+//         int count = 0;
+//         int majority = 0;
+//         while (left <= right) {
+//             if (nums[left] == nums[right]) {
+//                 count++;
+//                 majority = max(count, majority);
+//                 right++;
+//                 if (majority > n / 2) {
+//                     return nums[left];
+//                 }
+//             } else {
+//                 left = right;
+//                 count =0;
+//             }
+//         }
+//         return 0;
+//     }
+// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int left = 0;
-        int right = 0;
-        int count = 0;
-        int majority = 0;
-        while (left <= right) {
-            if (nums[left] == nums[right]) {
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
                 count++;
-                majority = max(count, majority);
-                right++;
-                if (majority > n / 2) {
-                    return nums[left];
-                }
+                if (count > n / 2)
+                    return nums[i];
             } else {
-                left = right;
-                count =0;
+                count = 1;
             }
         }
-        return 0;
+        return nums[0]; // handles n == 1
     }
 };
