98e664638f1402b9db4f0f4c9d4bb4a5b0c9c34f
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-12 17:46:35 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (42.70%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -17,32 +17,32 @@
 //     }
 // };
 
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
 
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int left = 0;
-        int right = 0;
-        int count = 0;
-        int majority = 0;
-        while (left <= right) {
-            if (nums[left] == nums[right]) {
-                count++;
-                majority = max(count, majority);
-                right++;
-                if (majority > n / 2) {
-                    return nums[left];
-                }
-            } else {
-                left = right;
-                count =0;
-            }
-        }
-        return 0;
-    }
-};
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         int left = 0;
+//         int right = 0;
+//         int count = 0;
+//         int majority = 0;
+//         while (left <= right) {
+//             if (nums[left] == nums[right]) {
+//                 count++;
+//                 majority = max(count, majority);
+//                 right++;
+//                 if (majority > n / 2) {
+//                     return nums[left];
+//                 }
+//             } else {
+//                 left = right;
+//                 count =0;
+//             }
+//         }
+//         return 0;
+//     }
+// };
 
 // class Solution {
 // public:
@@ -91,3 +91,28 @@ public:
 //         return -1;
 //     }
 // };
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0;
+        int cnt = 0;
+
+        for(int i : nums){
+            if(cnt == 0){
+                candidate = i;
+            }
+
+            if(i == candidate){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+
+        return candidate;
+        
+    }
+};
