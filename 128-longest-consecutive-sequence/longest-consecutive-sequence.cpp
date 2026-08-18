2cf46bea9d4b2791354d4362ab70621ee308446e
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-18 19:39:48 +0530

    Time: 82 ms (51.28%) | Memory: 89 MB (49.38%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -28,6 +28,36 @@
 //     }
 // };
 
+
+
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+
+//         int longest = 1;
+//         int cnt = 1;
+//         int last_smaller = INT_MIN;
+//         int n =nums.size();
+
+//         if(n == 0) return 0;
+
+//         sort(nums.begin(), nums.end());
+
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] - 1 == last_smaller) {
+//                 cnt++;
+//                 last_smaller = nums[i];
+//             }
+//             else if(nums[i] != last_smaller){
+//                 cnt =1;
+//                 last_smaller = nums[i];
+//             }
+//             longest = max(longest, cnt);
+//         }
+//         return longest;
+//     }
+// };
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
@@ -57,30 +87,4 @@ public:
     }
 };
 
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
 
-//         int longest = 1;
-//         int cnt = 1;
-//         int last_smaller = INT_MIN;
-//         int n =nums.size();
-
-//         if(n == 0) return 0;
-
-//         sort(nums.begin(), nums.end());
-
-//         for (int i = 0; i < n; i++) {
-//             if (nums[i] - 1 == last_smaller) {
-//                 cnt++;
-//                 last_smaller = nums[i];
-//             }
-//             else if(nums[i] != last_smaller){
-//                 cnt =1;
-//                 last_smaller = nums[i];
-//             }
-//             longest = max(longest, cnt);
-//         }
-//         return longest;
-//     }
-// };
