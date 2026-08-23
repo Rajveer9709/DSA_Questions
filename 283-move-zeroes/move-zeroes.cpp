c5e61e8673d47dba04460905daec2571d7676079
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-24 00:12:12 +0530

    Time: 3 ms (14.51%) | Memory: 23.7 MB (82.99%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -12,46 +12,46 @@
 //     }
 // };
 
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        vector<int> copy;
-        int counter = 0;
-        for(auto it : nums){
-            if(it != 0){
-                copy.emplace_back(it);
-            }
-            else{
-                counter++;
-            }
-        }
-        for(int i=0; i<counter; i++){
-            copy.push_back(0);
-        }
-
-        nums = copy;
-    }
-};
-
 // class Solution {
 // public:
 //     void moveZeroes(vector<int>& nums) {
-//         int j = -1;
-//         for (int i = 0; i < nums.size(); i++) {
-//             if (nums[i] == 0) {
-//                 j = i;
-//                 break;
+//         vector<int> copy;
+//         int counter = 0;
+//         for(auto it : nums){
+//             if(it != 0){
+//                 copy.emplace_back(it);
+//             }
+//             else{
+//                 counter++;
 //             }
 //         }
-
-//         if (j == -1) return; 
-
-//         for (int i = j + 1; i < nums.size(); i++) {
-//             if (nums[i] != 0) {
-//                 swap(nums[i], nums[j]);
-//                 j++;
-//             }
+//         for(int i=0; i<counter; i++){
+//             copy.push_back(0);
 //         }
+
+//         nums = copy;
 //     }
 // };
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        if (j == -1) return; 
+
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
 
