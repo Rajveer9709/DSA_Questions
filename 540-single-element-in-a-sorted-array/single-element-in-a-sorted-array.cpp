d384cebcb966c09be82dd9fc2d33c73eb09f2cb7
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-25 22:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (36.93%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -30,12 +30,37 @@
 //         return 0;
 //     }
 // };
+
+
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int result = 0;
+//         for (int num : nums)
+//             result ^= num;
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int result = 0;
-        for (int num : nums)
-            result ^= num;
-        return result;
+        int n = nums.size();
+        int low = 0;
+        int high = n - 2; // last valid "even" position to compare
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 1)
+                mid--; // force mid to be even
+
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2; // single element is after this pair
+            } else {
+                high = mid - 1; // single element is at or before mid
+            }
+        }
+        return nums[low];
     }
 };
