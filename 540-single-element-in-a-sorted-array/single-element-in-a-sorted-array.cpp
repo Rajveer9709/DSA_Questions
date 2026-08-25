0e8307f027326d907759e4e7320aca2aea7fd04d
Rajveer9707 <rajveernandkar@gmail.com>
2026-08-25 22:33:20 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (36.93%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,32 +1,41 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+
+//         int n = nums.size();
+//         int low = 0;
+//         int high = n - 1;
+
+//         while (low <= high) {
+//             int mid = low + (high - low) / 2;
+
+//             if (mid % 2 == 0) {
+//                 if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+//                     low = mid + 1;
+//                 } else if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
+//                     high = mid - 1;
+//                 } else {
+//                     return nums[mid];
+//                 }
+//             } else {
+//                 if (nums[mid] == nums[mid - 1]) {
+//                     low = mid + 1;
+//                 } else if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+//                     high = mid - 1;
+//                 } else {
+//                     return nums[mid];
+//                 }
+//             }
+//         }
+//         return 0;
+//     }
+// };
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-
-        int n = nums.size();
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (mid % 2 == 0) {
-                if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
-                    low = mid + 1;
-                } else if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
-                    high = mid - 1;
-                } else {
-                    return nums[mid];
-                }
-            } else {
-                if (nums[mid] == nums[mid - 1]) {
-                    low = mid + 1;
-                } else if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
-                    high = mid - 1;
-                } else {
-                    return nums[mid];
-                }
-            }
-        }
-        return 0;
+        int result = 0;
+        for (int num : nums)
+            result ^= num;
+        return result;
     }
 };
